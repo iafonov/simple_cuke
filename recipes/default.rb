6c74cb9d0074c34752225e702651143ad0960492
Igor Afonov <afonov@gmail.com>
2012-03-30 11:31:01 +0300

    Run through foodcritic.

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,19 +1,19 @@
-suite_path     = node["simple_cuke"]["suite_path"]
-handler_path   = node['chef_handler']['handler_path']
-reporters_path = File.join(handler_path, "reporters")
+suite_path     = node['simple_cuke']['suite_path']
+handler_path   = File.join(node['chef_handler']['handler_path'], 'cucumber_handler.rb')
+reporters_path = File.join(node['chef_handler']['handler_path'], 'reporters')
 
-cookbook_file File.join(handler_path, "cucumber_handler.rb") do
-  source "cucumber_handler.rb"
+cookbook_file handler_path do
+  source 'cucumber_handler.rb'
 end.run_action(:create)
 
-chef_handler "CucumberHandler" do
-  source File.join(handler_path, "cucumber_handler.rb")
-  action :enable
+chef_handler 'CucumberHandler' do
+  source handler_path
+  action 'enable'
   arguments :suite_path     => suite_path,
             :reporters_path => reporters_path,
-            :node_roles     => node.roles,
-            :all_roles      => search(:role, "name:*").map{ |role| role.name },
-            :reporter       => node["simple_cuke"]["reporter"]
+            :node_roles     => node['roles'],
+            :all_roles      => Chef::Config[:solo] ? [] : search(:role, 'name:*').map{ |role| role.name },
+            :reporter       => node['simple_cuke']['reporter']
 end
 
 remote_directory reporters_path do
@@ -29,10 +29,10 @@ remote_directory suite_path do
 end
 
 # code borrowed from https://github.com/btm/minitest-handler-cookbook/blob/master/recipes/default.rb
-node[:recipes].each do |recipe|
+node['recipes'].each do |recipe|
   cookbook_name = recipe.split('::').first
-  remote_directory "#{cookbook_name}" do
-    source "features"
+  remote_directory cookbook_name do
+    source 'features'
     cookbook cookbook_name
     path "#{suite_path}/features/#{cookbook_name}"
     purge true
