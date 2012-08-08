3603747d493d61fc7e00ecaf71d58c9b55afac62
Igor Afonov <afonov@gmail.com>
2012-08-08 18:19:24 +0300

    Fixed colored output.

--- files/default/reporters/console_reporter.rb
+++ files/default/reporters/console_reporter.rb
@@ -1,11 +1,20 @@
 class ConsoleReporter
   def success(report)
     puts report
-    puts 'Test Suite OK'
+    puts green('Test Suite OK')
   end
 
   def fail(report)
     puts report
-    puts 'Test Suite Failed'
+    puts red('Test Suite Failed')
+  end
+
+private
+
+  def red(text); colorize(text, "\e[31m"); end
+  def green(text); colorize(text, "\e[32m"); end
+
+  def colorize(text, color_code)
+    "#{color_code}#{text}\e[0m"
   end
 end
